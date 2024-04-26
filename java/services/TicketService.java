fc7391911d0ee9b7eff1c1630cf252a334253ccb
Harsh Panchal <harshkumarpanchal04@gmail.com>
2024-04-26 05:39:52 +0530

    All the functionalities added

---
+++ java/services/TicketService.java
@@ -0,0 +1,40 @@
+package services;
+
+
+
+import exceptions.InvalidGateException;
+import models.Gate;
+import models.Ticket;
+import models.VehicleType;
+import repositories.GateRepository;
+
+import java.util.Date;
+import java.util.Optional;
+
+public class TicketService {
+    private GateRepository gateRepository;
+
+    public TicketService(GateRepository gateRepository) {
+        this.gateRepository = gateRepository;
+    }
+
+    public Ticket issueTicket(Long gateId, String vehicleNumber, VehicleType vehicleType, String ownerName) throws InvalidGateException {
+        Ticket ticket = new Ticket();
+        ticket.setEntryTime(new Date());
+
+        Optional<Gate> optionalGate = gateRepository.findById(gateId);
+
+        if (optionalGate.isEmpty()) {
+            throw new InvalidGateException("Invalid gate Id");
+        }
+
+        Gate gate = optionalGate.get();
+        ticket.setGeneratedAt(gate);
+        ticket.setGeneratedBy(gate.getOperator());
+
+
+
+
+        return null;
+    }
+}
